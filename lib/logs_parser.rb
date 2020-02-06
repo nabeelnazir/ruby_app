3a07ad6e8b6e3a837170ddf7c9fab8ab9e58a0b6
nabeelnazir <nabeeln796@yahoo.com>
2020-02-06 17:18:02 +0500

    seperate class added for logs validations

--- lib/logs_parser.rb
+++ lib/logs_parser.rb
@@ -1,10 +1,10 @@
+require_relative './logs_validation.rb'
 class LogsParser
-  PAGE = %r{(?<path>[a-zA-Z0-9\/_-]*)}.freeze # page rejex. for example '/home/1'
-  IP = /(?<ip>([0-9]{1,3}\.){3}([0-9]{1,3}){1})/.freeze # IP rejex.
-
+  
   def initialize(logfile_path)
     @logfile_path = logfile_path
-    validate_file # check file exists and it should not be empty.
+    @logs_validation = LogsValidation.new(logfile_path)
+    @logs_validation.validate_file
     @logs = {}
   end
 
@@ -14,29 +14,12 @@ class LogsParser
       data = line.split(' ')
       page = data.first
       ip = data.last
-      unless validate_log_entry(page, ip)
-        invalid_log_entry(line)
+      unless @logs_validation.validate_log_entry(page, ip)
+        @logs_validation.invalid_log_entry(line)
       end # chek if log entry is valid.
       @logs[page] ? @logs[page] << ip : @logs[page] = [ip]
     end
     @logs
   end
 
-  private
-
-  def validate_file
-    unless File.exist?(@logfile_path)
-      raise "No such file or directory @ #{@logfile_path}"
-    end
-    raise 'File is empty' if File.zero?(@logfile_path)
-  end
-
-  # check if IP and Page url ('/home/1') is valid.
-  def validate_log_entry(page, ip)
-    !!(IP =~ ip) && !!(PAGE =~ page)
-  end
-
-  def invalid_log_entry(line)
-    raise "Log entry is invalid: #{line}"
-  end
 end
