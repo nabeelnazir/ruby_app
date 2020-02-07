d2899c826430b7149b9dddd592e8896dd38eabf7
nabeelnazir <nabeeln796@yahoo.com>
2020-02-07 15:27:26 +0500

    ruby option parser added

--- parser.rb
+++ parser.rb
@@ -1,8 +1,22 @@
 require_relative 'lib/logs_parser.rb'
 require_relative 'lib/logs_view.rb'
 require 'table_print'
+require 'optparse'
 
-logs = LogsParser.new(ARGV[0]).parse_logs
+############################# OptionParser ##############################
+options = {}
+
+optparse = OptionParser.new do |opts|
+  opts.on('-f', '--file_path FILENAME', 'Mandatory File Path') do |f|
+    options[:file_path] = f
+  end
+end
+
+optparse.parse!
+
+############################# OptionParser ##############################
+
+logs = LogsParser.new(options[:file_path]).parse_logs
 logs_view = LogsView.new(logs)
 
 puts
